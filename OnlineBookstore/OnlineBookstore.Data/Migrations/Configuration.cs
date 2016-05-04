2a30aeb997acd9d8f1b191346c81dccdd5f455d0
GDimitrov <Themikuma@gmail.com>
2016-05-04 23:06:07 +0200

    Architecture changes
    Split the project into several projects. There is a project for models
    and one for Data now. Extended the default user with first name and last
    name and enabled migrations.

---
+++ OnlineBookstore/OnlineBookstore.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace OnlineBookstore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UserDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "UserDbContext";
+        }
+
+        protected override void Seed(UserDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ OnlineBookstore/OnlineBookstore.Data/UserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OnlineBookstore.Data.Migrations;
+using OnlineBookstore.Models;
+using System.Data.Entity;
+
+namespace OnlineBookstore.Data
+{
+    public class UserDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public UserDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion
+                <UserDbContext, Configuration>());
+        }
+
+        public static UserDbContext Create()
+        {
+            return new UserDbContext();
+        }
+    }
+}
