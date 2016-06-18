5f74eb1f408a02b5f6b2ae397124f29ea7af616e
GDimitrov <Themikuma@gmail.com>
2016-06-18 14:11:25 +0200

    Merge problems

--- OnlineBookstore/OnlineBookstore.Data/UserDbContext.cs
+++ OnlineBookstore/OnlineBookstore.Data/UserDbContext.cs
@@ -1,27 +1,23 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using OnlineBookstore.Data.Migrations;
 using OnlineBookstore.Models;
-using OnlineBookstore.Models.BookstoreModels;
 using System.Data.Entity;
 
 namespace OnlineBookstore.Data
 {
-    public class DbContext : IdentityDbContext<ApplicationUser>
+    public class UserDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbContext()
+        public UserDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
             Database.SetInitializer(
                 new MigrateDatabaseToLatestVersion
-                <DbContext, UserConfiguration>());
+                <UserDbContext, UserConfiguration>());
         }
-        public virtual DbSet<Book> Books { get; set; }
 
-        public virtual DbSet<OrderItem> Orders { get; set; }
-
-        public static DbContext Create()
+        public static UserDbContext Create()
         {
-            return new DbContext();
+            return new UserDbContext();
         }
     }
 }
