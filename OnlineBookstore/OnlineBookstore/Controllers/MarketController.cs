9c6206ffea0908ae980206f7e88fc9eee5729b24
GDimitrov <Themikuma@gmail.com>
2016-06-20 08:19:47 +0200

    Authorization
    Made the market controller for authorized visitors. Only the page
    preview is for non authorized.

--- OnlineBookstore/OnlineBookstore/Controllers/MarketController.cs
+++ OnlineBookstore/OnlineBookstore/Controllers/MarketController.cs
@@ -8,9 +8,11 @@ using System.Web.Mvc;
 
 namespace OnlineBookstore.Controllers
 {
+    [Authorize]
     public class MarketController : Controller
     {
         // GET: Market
+        [AllowAnonymous]
         public ActionResult MarketPage(int? filterBy, string filterText)
         {
             Repository repository = new Repository();
@@ -38,7 +40,7 @@ namespace OnlineBookstore.Controllers
             model.BestSellers = new List<BookPreviewViewModel>();
             return View(model);
         }
-        [Authorize]
+        
         public ActionResult CartList()
         {
             Repository repository = new Repository();
@@ -46,51 +48,33 @@ namespace OnlineBookstore.Controllers
 
             return View(model);
         }
-
-        // Bring the books from cartlist to orderlist, this is the example
-        public OrderPageViewModel addOrderList()
-        {
-            List<BookPreviewViewModel> OrderList = new List<BookPreviewViewModel>();
-            OrderList.Add(new BookPreviewViewModel { Description = "Fiction", Picture = "HolderCover.jpg", Price = 50, Stars = 4, Title = "Fiction One", Quantity = 3 });
-            OrderList.Add(new BookPreviewViewModel { Description = "More Fiction", Picture = "shardblade.jpg", Price = 32.64, Stars = 1, Title = "Fiction Two", Quantity = 5 });
-            OrderPageViewModel model = new OrderPageViewModel();
-            model.OrderPageModel = OrderList;
-            model.TotalPrice = 0;
-            return model;
-        }
-
+        
         public ActionResult OrderPage()
         {
             Repository repository = new Repository();
-            repository.FinishOrder(User.Identity.GetUserId());
-            OrderPageViewModel model = addOrderList();
-
-            foreach (var item in model.OrderPageModel)
-            {
-                double price = item.Price;
-                model.TotalPrice = model.TotalPrice + price;
-            }
+            OrderPageViewModel model = repository.GetUserData(User.Identity.GetUserId());
+            model.Items = repository.GetUnfinishedOrders(User.Identity.GetUserId());
             return View(model);
         }
 
-<<<<<<< HEAD
         public ActionResult DeleteCartItem(int id)
         {
             Repository repository = new Repository();
             repository.DeleteCartItem(id);
             return RedirectToAction("CartList");
         }
-
+        
         public ActionResult OrderBook(string isbn)
         {
             Repository repository = new Repository();
             repository.CreateOrder(User.Identity.GetUserId(), isbn);
             return RedirectToAction("CartList");
-=======
+        }
         public ActionResult OrderSuccess()
         {
+            Repository repository = new Repository();
+            repository.FinishOrder(User.Identity.GetUserId());
             return View();
->>>>>>> b6845f9508b691328831f6da472e5b02e6b85c7b
         }
 
     }
